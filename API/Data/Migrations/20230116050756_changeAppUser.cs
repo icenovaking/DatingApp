0dc74c633e243dab7b6336be4531fa6ec89a7777
沂叡 莊 <iceorbbroy@gmail.com>
2023-01-16 18:33:14 +0800

    1/16

---
+++ API/Data/Migrations/20230116050756_changeAppUser.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class changeAppUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UseName",
+                table: "Users",
+                newName: "UserName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Users",
+                newName: "UseName");
+        }
+    }
+}
